8f85e700b50bfb17635a63cb5d86866dd8800c04
jjurman <jjurman@constantcontact.com>
2017-09-24 19:27:37 -0400

    new video tutorial

--- bundle.js
+++ bundle.js
@@ -1404,16 +1404,16 @@ module.exports = (attrs, children) => {
   return html`
     <tcr title="Video Tutorial" color=${attrs.color} bg=${attrs.bg}>
       <div style=${summaryStyle}>
-        <a href="https://youtu.be/mgHJbqls-wk">
+        <a href="https://youtu.be/Y00fakErEQk">
           <img src="/youtube-video-thumbnail.png" width="100%">
         </a>
       </div>
       <div style=${summaryStyle}>
         You can now watch a video tutorial that goes though the process
-        of building a Tram-One app, from start to finish (does not use tram-one-express).
+        of building a Tram-One app, from start to finish.
         <br><br>
         You can watch it on youtube here:
-        <a href="https://youtu.be/mgHJbqls-wk">https://youtu.be/mgHJbqls-wk</a>
+        <a href="https://youtu.be/Y00fakErEQk">https://youtu.be/Y00fakErEQk</a>
       </div>
     </tcr>
   `
