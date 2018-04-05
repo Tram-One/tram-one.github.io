fecf7f408422da3893b76d0788f98bba783caf4c
Jesse Jurman <j.r.jurman@gmail.com>
2018-04-05 01:38:02 -0400

    removed extrenous code

--- main.js
+++ main.js
@@ -1,5 +1,25 @@
 const Tram = require('tram-one')
-
+const html = Tram.html({
+  'app-header': require('./components/app-header'),
+  'introduction': require('./sections/introduction/introduction'),
+  'features': require('./sections/features/features'),
+  'api': require('./sections/api/api'),
+  'install': require('./sections/install/install')
+})
 const app = new Tram({defaultRoute: '/'})
-app.addRoute('/', require('./pages/app'))
+
+const page = () => {
+  return html`
+    <div>
+      <app-header />
+      <introduction />
+      <features />
+      <install />
+      <api />
+    </div>
+  `
+}
+
+
+app.addRoute('/', page)
 app.start('.main')
