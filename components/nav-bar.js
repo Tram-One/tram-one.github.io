0da835d0b37021d19d3dc9a20be4e0144206137e
Jesse Jurman <j.r.jurman@gmail.com>
2018-04-05 02:55:50 -0400

    remove remix...for now

--- components/nav-bar.js
+++ components/nav-bar.js
@@ -21,7 +21,6 @@ module.exports = (attrs) => {
       <scroll-anchor style=${linkStyle} href="#features">Features</scroll-anchor>
       <scroll-anchor style=${linkStyle} href="#install">Install</scroll-anchor>
       <scroll-anchor style=${linkStyle} href="#api">API</scroll-anchor>
-      <nav-link style=${linkStyle} href="#remix">Remix</nav-link>
       <nav-link style=${linkStyle} href="https://join.slack.com/t/tram-one/shared_invite/enQtMjY0NDA3OTg2MzQyLWUyMGIyZTYwNzZkNDJiNWNmNzdiOTMzYjg0YzMzZTkzZDE4MTlmN2Q2YjE0NDIwMGI3ODEzYzQ4ODdlMzQ2ODM">Slack</nav-link>
       <nav-link style=${linkStyle} href="https://github.com/Tram-One">Github</nav-link>
       <nav-link style=${linkStyle} href="https://www.npmjs.com/package/tram-one">NPM</nav-link>
