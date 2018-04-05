fbb2b886f58e9a17d570ae2c8eb422edfd2260f8
Jesse Jurman <j.r.jurman@gmail.com>
2018-04-05 01:23:06 -0400

    no wrapping... at all

--- components/nav-bar.js
+++ components/nav-bar.js
@@ -6,13 +6,12 @@ const html = Tram.html({
 
 const navGrid = `
   display: flex;
-  flex-wrap: wrap;
+  flex-wrap: no wrap;
   margin-bottom: 0.8em;
 `
 
 const linkStyle = `
   padding: 0.4em 0.8em 0em 0.0em;
-  white-space: nowrap;
 `
 
 module.exports = (attrs) => {
@@ -23,10 +22,9 @@ module.exports = (attrs) => {
       <scroll-anchor style=${linkStyle} href="#api">API</scroll-anchor>
       <scroll-anchor style=${linkStyle} href="#install">Install</scroll-anchor>
       <nav-link style=${linkStyle} href="#remix">Remix</nav-link>
-      <div>
-        <nav-link style=${linkStyle} href="https://join.slack.com/t/tram-one/shared_invite/enQtMjY0NDA3OTg2MzQyLWUyMGIyZTYwNzZkNDJiNWNmNzdiOTMzYjg0YzMzZTkzZDE4MTlmN2Q2YjE0NDIwMGI3ODEzYzQ4ODdlMzQ2ODM">Slack</nav-link>
-        <nav-link style=${linkStyle} href="https://github.com/Tram-One">Github</nav-link>
-      </div>
+      <nav-link style=${linkStyle} href="https://join.slack.com/t/tram-one/shared_invite/enQtMjY0NDA3OTg2MzQyLWUyMGIyZTYwNzZkNDJiNWNmNzdiOTMzYjg0YzMzZTkzZDE4MTlmN2Q2YjE0NDIwMGI3ODEzYzQ4ODdlMzQ2ODM">Slack</nav-link>
+      <nav-link style=${linkStyle} href="https://github.com/Tram-One">Github</nav-link>
+      <nav-link style=${linkStyle} href="https://www.npmjs.com/package/tram-one">NPM</nav-link>
     </div>
   `
 }
