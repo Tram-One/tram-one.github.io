77e9660d9bf77fe0cb64c69c0f1378f602f6ac80
jjurman <jjurman@constantcontact.com>
2018-06-22 03:49:08 -0400

    stackblitz link

--- components/nav-bar.js
+++ components/nav-bar.js
@@ -14,6 +14,11 @@ const linkStyle = `
   padding: 0.4em 0.8em 0em 0.0em;
 `
 
+const externalStyle = `
+  padding: 0.4em 0.8em 0em 0.0em;
+  color: #3939ab;
+`
+
 module.exports = (attrs) => {
   return html`
     <div style="${navGrid}${attrs.style}">
@@ -21,9 +26,10 @@ module.exports = (attrs) => {
       <scroll-anchor style=${linkStyle} href="#features">Features</scroll-anchor>
       <scroll-anchor style=${linkStyle} href="#install">Install</scroll-anchor>
       <scroll-anchor style=${linkStyle} href="#api">API</scroll-anchor>
-      <nav-link style=${linkStyle} href="https://join.slack.com/t/tram-one/shared_invite/enQtMjY0NDA3OTg2MzQyLWUyMGIyZTYwNzZkNDJiNWNmNzdiOTMzYjg0YzMzZTkzZDE4MTlmN2Q2YjE0NDIwMGI3ODEzYzQ4ODdlMzQ2ODM">Slack</nav-link>
-      <nav-link style=${linkStyle} href="https://github.com/Tram-One">Github</nav-link>
-      <nav-link style=${linkStyle} href="https://www.npmjs.com/package/tram-one">NPM</nav-link>
+      <nav-link style=${externalStyle} href="https://join.slack.com/t/tram-one/shared_invite/enQtMjY0NDA3OTg2MzQyLWUyMGIyZTYwNzZkNDJiNWNmNzdiOTMzYjg0YzMzZTkzZDE4MTlmN2Q2YjE0NDIwMGI3ODEzYzQ4ODdlMzQ2ODM">Slack</nav-link>
+      <nav-link style=${externalStyle} href="https://github.com/Tram-One">Github</nav-link>
+      <nav-link style=${externalStyle} href="https://www.npmjs.com/package/tram-one">NPM</nav-link>
+      <nav-link style=${externalStyle} href="https://stackblitz.com/edit/tram-one">StackBlitz</nav-link>
     </div>
   `
 }
